1c3ebb78225255a71995ea138d6eaca176467554
MhozaifaA <botchey.mha@hotmail.com>
2022-01-03 21:41:16 +0200

    Updates

---
+++ precache-manifest.6a17224d03544a1a73870a8c62081cd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a9640549f2bb2cfb02bc93db0bee40",
+    "url": "/ninjakick/index.html"
+  },
+  {
+    "revision": "0dfbf8d41fb31515de7c",
+    "url": "/ninjakick/static/css/2.ff29d27e.chunk.css"
+  },
+  {
+    "revision": "c760061b63328e95fef0",
+    "url": "/ninjakick/static/css/main.ecfa3bbf.chunk.css"
+  },
+  {
+    "revision": "0dfbf8d41fb31515de7c",
+    "url": "/ninjakick/static/js/2.3f14c220.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/ninjakick/static/js/2.3f14c220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c760061b63328e95fef0",
+    "url": "/ninjakick/static/js/main.45f027d9.chunk.js"
+  },
+  {
+    "revision": "51fc0c4b089dd064dc09",
+    "url": "/ninjakick/static/js/runtime-main.4604251d.js"
+  }
+]);
