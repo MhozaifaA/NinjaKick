83891b413e8a034ac715e9ffbcf9f9336344a8bb
MhozaifaA <botchey.mha@hotmail.com>
2022-01-03 23:41:17 +0200

    Updates

---
+++ precache-manifest.eb86f5ed8394fb3a234d95d98ea43aa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5211e903f8c6c1ae7add0b44203c43b8",
+    "url": "/NinjaKick/index.html"
+  },
+  {
+    "revision": "2d8492a71aa936e1dbf9",
+    "url": "/NinjaKick/static/css/2.ff29d27e.chunk.css"
+  },
+  {
+    "revision": "8445e3fa73416f8713ba",
+    "url": "/NinjaKick/static/css/main.ecfa3bbf.chunk.css"
+  },
+  {
+    "revision": "2d8492a71aa936e1dbf9",
+    "url": "/NinjaKick/static/js/2.373895a0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/NinjaKick/static/js/2.373895a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8445e3fa73416f8713ba",
+    "url": "/NinjaKick/static/js/main.8c10dfe0.chunk.js"
+  },
+  {
+    "revision": "2ce081594d7df0427c4c",
+    "url": "/NinjaKick/static/js/runtime-main.c917d36d.js"
+  }
+]);
